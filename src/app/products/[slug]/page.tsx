0a290d1d42073f50f0442f4484b3d972a06d5dbb
Muhammad Shakeel Niaamat <shakeel.niaamat@systemsltd.com>
2023-10-03 17:35:29 +0500

    prisma fix

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -3,12 +3,21 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
+const fetchProduct = async (idProduct: string) => {
+  try {
+    return await prisma.product.findMany({
+      where: { id: idProduct },
+    });
+  } catch (error) {
+    return [];
+  }
+};
+
 async function Product(props: any) {
   const idProduct = props.params.slug;
 
-  const product = await prisma.product.findMany({
-    where: { id: idProduct },
-  });
+  const product = await fetchProduct(idProduct);
+
   return <ProductDetail item={product[0]} />;
 }
 
--- src/app/util/server/prismaQuery.tsx
+++ src/app/util/server/prismaQuery.tsx
@@ -2,48 +2,56 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export const fetchCategory = async () => {
-  const cat = await prisma.category.findMany({
-    select: {
-      id: true,
-      name: true,
-    },
-  });
-  return cat;
+  try {
+    const cat = await prisma.category.findMany({
+      select: {
+        id: true,
+        name: true,
+      },
+    });
+    return cat;
+  } catch (error) {
+    return [];
+  }
 };
 
 export const fetchProduct = async (
   byeCateGoryId: number | null,
   searchKey: string | null
 ) => {
-  let result;
-  if (byeCateGoryId) {
-    result = await prisma.product.findMany({
-      where: {
-        category: {
-          every: {
-            id: +byeCateGoryId,
+  try {
+    let result;
+    if (byeCateGoryId) {
+      result = await prisma.product.findMany({
+        where: {
+          category: {
+            every: {
+              id: +byeCateGoryId,
+            },
           },
         },
-      },
-    });
-  } else if (searchKey) {
-    result = await prisma.product.findMany({
-      where: {
-        title: {
-          search: searchKey,
+      });
+    } else if (searchKey) {
+      result = await prisma.product.findMany({
+        where: {
+          title: {
+            search: searchKey,
+          },
         },
-      },
-      select: {
-        id: true,
-        priceInCents: true,
-        title: true,
-        image: true,
-        description: true,
-        category: true,
-      },
-    });
-  } else {
-    result = await prisma.product.findMany();
+        select: {
+          id: true,
+          priceInCents: true,
+          title: true,
+          image: true,
+          description: true,
+          category: true,
+        },
+      });
+    } else {
+      result = await prisma.product.findMany();
+    }
+    return result;
+  } catch (error) {
+    return [];
   }
-  return result;
 };
