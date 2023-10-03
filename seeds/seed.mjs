13246f983c3d3140ac831a695b70270f70a9ffdb
Muhammad Shakeel Niaamat <shakeel.niaamat@systemsltd.com>
2023-10-03 19:27:17 +0500

    prisma fix

--- seeds/seed.mjs
+++ seeds/seed.mjs
@@ -31,12 +31,78 @@ async function main() {
             create: [
               {
                 priceInCents: "1000",
-                title: "Mesh Panel Ruched Glitter Top",
-                image: "1668320696/brand/naqzoqkkf5ieby7pyros",
-                description: "Slim fit regular sleeve mesh panel",
+                title: "L'Oreal Paris",
+                image:
+                  "https://media.naheed.pk/catalog/product/cache/ed9f5ebe2a117625f6cd6336daddd764/1/2/1261594-2.jpg",
+                description:
+                  "L'Oreal Paris Innovation Revitalift Hyaluronic Acid 1.5% Hyaluron Serum, 30ml",
                 category: {
                   connect: {
-                    id: 2,
+                    id: 1,
+                  },
+                },
+              },
+              {
+                priceInCents: "620",
+                title: "Issey Miyake",
+                image:
+                  "https://media.naheed.pk/catalog/product/cache/a05e171a5a1348658a0feb46a8ffae67/1/2/1260791-2.jpg",
+                description:
+                  "Issey Miyake L'Eau D'Issey Set, For Women, Eau De Toilette 100 + Eau De Toilette 10ml + Body Lotion 50ml",
+                category: {
+                  connect: {
+                    id: 1,
+                  },
+                },
+              },
+              {
+                priceInCents: "400",
+                title: "Burberry ",
+                image:
+                  "https://media.naheed.pk/catalog/product/cache/a05e171a5a1348658a0feb46a8ffae67/1/2/1260665-1.jpg",
+                description:
+                  "Burberry Brit Splash For Him Eau De Toilette, For Men, 100ml",
+                category: {
+                  connect: {
+                    id: 1,
+                  },
+                },
+              },
+              {
+                priceInCents: "900",
+                title: "Asgharali",
+                image:
+                  "https://media.naheed.pk/catalog/product/cache/a05e171a5a1348658a0feb46a8ffae67/1/2/1260956-1.jpg",
+                description: "Asghar Ali Eclipse Eau De Parfum, For Men, 100ml",
+                category: {
+                  connect: {
+                    id: 1,
+                  },
+                },
+              },
+              {
+                priceInCents: "150",
+                title: "L'Oreal Paris",
+                image:
+                  "https://media.naheed.pk/catalog/product/cache/ed9f5ebe2a117625f6cd6336daddd764/1/2/1261594-2.jpg",
+                description:
+                  "L'Oreal Paris Innovation Revitalift Hyaluronic Acid 1.5% Hyaluron Serum, 30ml",
+                category: {
+                  connect: {
+                    id: 1,
+                  },
+                },
+              },
+              {
+                priceInCents: "1000",
+                title: "L'Oreal Paris",
+                image:
+                  "https://media.naheed.pk/catalog/product/cache/ed9f5ebe2a117625f6cd6336daddd764/1/2/1261594-2.jpg",
+                description:
+                  "L'Oreal Paris Innovation Revitalift Hyaluronic Acid 1.5% Hyaluron Serum, 30ml",
+                category: {
+                  connect: {
+                    id: 1,
                   },
                 },
               },
@@ -90,13 +156,77 @@ async function main() {
               },
               {
                 priceInCents: "1000",
-                title: "Mesh Panel Flounce Sleeve Blouse",
+                title: "Nespresso",
                 image:
-                  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOsTm-T9Xp4NszdEXKfZombe5LNgYUvBiuEw&usqp=CAU",
-                description: "Ruffle contrast mesh with flounce sleeves",
+                  "https://media.naheed.pk/catalog/product/cache/a05e171a5a1348658a0feb46a8ffae67/1/2/1261516-1.jpg",
+                description:
+                  "Nespresso Creatista Pro Machine, CP-PK-Aluminum-IL-NE (SNE900)",
                 category: {
                   connect: {
-                    id: 2,
+                    id: 5,
+                  },
+                },
+              },
+              {
+                priceInCents: "110",
+                title: "Cofee machine",
+                image:
+                  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtpKmdwtgwHonEiPf4oYfPYIZNvcweHnr6nA&usqp=CAU",
+                description: "Nespresso Zenius Pro Machine, Matt Black, 9737",
+                category: {
+                  connect: {
+                    id: 5,
+                  },
+                },
+              },
+              {
+                priceInCents: "150",
+                title: "Sayona",
+                image:
+                  "https://media.naheed.pk/catalog/product/cache/a05e171a5a1348658a0feb46a8ffae67/1/2/1260775-1.jpg",
+                description:
+                  "Sayona Hand Mixer With Rotating Bowl, 4 Liters, 250-300W, SHM-4358",
+                category: {
+                  connect: {
+                    id: 5,
+                  },
+                },
+              },
+              {
+                priceInCents: "1100",
+                title: "Nespresso",
+                image:
+                  "https://media.naheed.pk/catalog/product/cache/a05e171a5a1348658a0feb46a8ffae67/1/2/1261513-1.jpg",
+                description: "Nespresso Gran Lattissima Machine, F531-ME-WH-NE",
+                category: {
+                  connect: {
+                    id: 5,
+                  },
+                },
+              },
+              {
+                priceInCents: "1100",
+                title: "Apollo Sports",
+                image:
+                  "https://media.naheed.pk/catalog/product/cache/a05e171a5a1348658a0feb46a8ffae67/1/2/1245667.jpg",
+                description:
+                  "Wish Alumtec 780 Badminton Racket, Green/White, 022474",
+                category: {
+                  connect: {
+                    id: 6,
+                  },
+                },
+              },
+              {
+                priceInCents: "1100",
+                title: "Apollo Sports",
+                image:
+                  "https://media.naheed.pk/catalog/product/cache/a05e171a5a1348658a0feb46a8ffae67/1/2/1245669.jpg",
+                description:
+                  "Wish Alumtec 550 Badminton Racket, Orange/White, 019813",
+                category: {
+                  connect: {
+                    id: 6,
                   },
                 },
               },
@@ -112,6 +242,71 @@ async function main() {
                   },
                 },
               },
+              {
+                priceInCents: "1100",
+                title: "MCD Leather Skipping Rope",
+                image:
+                  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtpKmdwtgwHonEiPf4oYfPYIZNvcweHnr6nA&usqp=CAU",
+                description:
+                  "tock: In Stock Ships In: 1-3 Days Delivery Area: Nationwide Country of Origin: Pakistan Shipped By: Naheed",
+                category: {
+                  connect: {
+                    id: 6,
+                  },
+                },
+              },
+              {
+                priceInCents: "1100",
+                title: "Freshly",
+                image:
+                  "https://media.naheed.pk/catalog/product/cache/a05e171a5a1348658a0feb46a8ffae67/1/2/1222778-1.jpg",
+                description: "Freshly Butter Microwave Popcorn, 297g",
+                category: {
+                  connect: {
+                    id: 7,
+                  },
+                },
+              },
+              {
+                priceInCents: "1100",
+                title: "Clean & Clear",
+                image:
+                  "https://media.naheed.pk/catalog/product/cache/a05e171a5a1348658a0feb46a8ffae67/1/2/1237180-1.jpg",
+                description:
+                  "Clean & Clear Maximum Strength Fragrance-Free Persa-Gel 10, 28g",
+                category: {
+                  connect: {
+                    id: 7,
+                  },
+                },
+              },
+              {
+                priceInCents: "1100",
+                title: "Samsung",
+                image:
+                  "https://media.naheed.pk/catalog/product/cache/a05e171a5a1348658a0feb46a8ffae67/1/2/1232140-1.jpg",
+                description:
+                  "Samsung Galaxy S22 Ultra 12/256GB Smart Phone, Burgundy",
+                category: {
+                  connect: {
+                    id: 7,
+                  },
+                },
+              },
+              {
+                priceInCents: "1100",
+                title: "Samsung Galaxy Z Flip 4 8GB/256GB Smartphone, Purple ",
+                image:
+                  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtpKmdwtgwHonEiPf4oYfPYIZNvcweHnr6nA&usqp=CAU",
+                description:
+                  "Side-mounted fingerprint reader Android 11 operating system- highly compatible with the latest software Battery capacity is around 3700 mAh- long-lasting power and support Fast battery charging and wireless charging Accelerometer, a barometer, a compass, a gyroscope, and a proximity sensor",
+                category: {
+                  connect: {
+                    id: 7,
+                  },
+                },
+              },
+
               {
                 priceInCents: "14600",
                 title: "N°5 Eau de Parfum Spray",
---
+++ src/app/about/page.tsx
@@ -0,0 +1,136 @@
+export default async function About() {
+  return (
+    <section className="flex items-center bg-stone-100 xl:h-screen font-poppins dark:bg-gray-800 ">
+      <div className="justify-center flex-1 max-w-6xl py-4 mx-auto lg:py-6 md:px-6">
+        <div className="flex flex-wrap ">
+          <div className="w-full px-4 mb-10 lg:w-1/2 lg:mb-0">
+            <img
+              src="https://clipart-library.com/2023/E-Commerce-Logo-PNG-Clipart-Background.png"
+              alt=""
+              className="relative z-40 object-cover w-full h-96 rounded-3xl"
+            />
+          </div>
+          <div className="w-full px-4 mb-10 lg:w-1/2 lg:mb-0 ">
+            <h2 className="mb-4 text-4xl font-semibold text-blue-500 dark:text-gray-300">
+              About Us
+            </h2>
+            <p className="mb-10 text-base leading-7 text-gray-500 dark:text-gray-400">
+              Last updated: November 13, 2018 These Terms and Conditions
+              ("Terms", "Terms and Conditions") govern your relationship with
+              naheed.pk website (the "Service") operated by Naheed.pk ("us",
+              "we", or "our"). Please read these Terms and Conditions carefully
+              before using the Service. Your access to and use of the Service is
+              conditioned on your acceptance of and compliance with these Terms.
+              These Terms apply to all visitors, users and others who access or
+              use the Service. By accessing or using the Service you agree to be
+              bound by these Terms. If you disagree with any part of the terms
+              then you may not access the Service. Purchases If you wish to
+              purchase any product or service made available through the Service
+              ("Purchase"), you may be asked to supply certain information
+              relevant to your Purchase including, without limitation, your
+              credit card number, the expiration date of your credit card, your
+              billing address, and your shipping information. In certain cases,
+              we may ask for additional informtation or documents to process
+              your order. You represent and warrant that: (i) you have the legal
+              right to use any credit card(s) or other payment method(s) in
+              connection with any Purchase; and that (ii) the information you
+              supply to us is true, correct and complete. By submitting such
+              information, you grant us the right to provide the information to
+              third parties for purposes of facilitating the completion of
+              Purchases. We reserve the right to refuse or cancel your order at
+              any time for certain reasons including but not limited to: product
+              or service availability, errors in the description or price of the
+              product or service, error in your order or other reasons. We
+              reserve the right to refuse or cancel your order if fraud or an
+              unauthorized or illegal transaction is suspected. In the event of
+              a need for refund of any transaction conducted through a debit or
+              credit card, the refund will be issued directly to the original
+              method of payment. We do not offer cash refunds for transactions
+              completed via debit or credit cards. You acknowledge that these
+              terms will apply irrespective of the reason for the refund,
+              including but not limited to, cancellation of an order by us due
+              to product or service availability, errors in the description or
+              price of the product or service, error in your order or any other
+              reasons. The time it may take for your refunded amount to appear
+              on your card statement can vary, please contact your card issuer
+              to learn about their policy regarding refund timings.
+              Availability, Errors and Inaccuracies We are constantly updating
+              our offerings of products and services on the Service. The
+              products or services available on our Service may be mispriced,
+              described inaccurately, or unavailable, and we may experience
+              delays in updating information on the Service and in our
+              advertising on other web sites. We cannot and do not guarantee the
+              accuracy or completeness of any information, including prices,
+              product images, specifications, availability, and services. We
+              reserve the right to change or update information and to correct
+              errors, inaccuracies, or omissions at any time without prior
+              notice. Contests, Sweepstakes and Promotions Any contests,
+              sweepstakes or other promotions (collectively, "Promotions") made
+              available through the Service may be governed by rules that are
+              separate from these Terms. If you participate in any Promotions,
+              please review the applicable rules as well as our Privacy Policy.
+              If the rules for a Promotion conflict with these Terms, the
+              Promotion rules will apply. Accounts When you create an account
+              with us, you must provide us information that is accurate,
+              complete, and current at all times. Failure to do so constitutes a
+              breach of the Terms, which may result in immediate termination of
+              your account on our Service. You are responsible for safeguarding
+              the password that you use to access the Service and for any
+              activities or actions under your password, whether your password
+              is with our Service or a third-party service. You agree not to
+              disclose your password to any third party. You must notify us
+              immediately upon becoming aware of any breach of security or
+              unauthorized use of your account. You may not use as a username
+              the name of another person or entity or that is not lawfully
+              available for use, a name or trade mark that is subject to any
+              rights of another person or entity other than you without
+              appropriate authorization, or a name that is otherwise offensive,
+              vulgar or obscene. Intellectual Property The Service and its
+              original content, features and functionality are and will remain
+              the exclusive property of Naheed.pk and its licensors. The Service
+              is protected by copyright, trademark, and other laws of both the
+              Pakistan and foreign countries. Our trademarks and trade dress may
+              not be used in connection with any product or service without the
+              prior written consent of Naheed.pk. Links To Other Web Sites Our
+              Service may contain links to third-party web sites or services
+              that are not owned or controlled by Naheed.pk. Naheed.pk has no
+              control over, and assumes no responsibility for, the content,
+              privacy policies, or practices of any third party web sites or
+              services. You further acknowledge and agree that Naheed.pk shall
+              not be responsible or liable, directly or indirectly, for any
+              damage or loss caused or alleged to be caused by or in connection
+              with use of or reliance on any such content, goods or services
+              available on or through any such web sites or services. We
+              strongly advise you to read the terms and conditions and privacy
+              policies of any third-party web sites or services that you visit.
+              Termination We may terminate or suspend your account immediately,
+              without prior notice or liability, for any reason whatsoever,
+              including without limitation if you breach the Terms. Upon
+              termination, your right to use the Service will immediately cease.
+              If you wish to terminate your account, you may simply discontinue
+              using the Service. Governing Law These Terms shall be governed and
+              construed in accordance with the laws of Pakistan, without regard
+              to its conflict of law provisions. Our failure to enforce any
+              right or provision of these Terms will not be considered a waiver
+              of those rights. If any provision of these Terms is held to be
+              invalid or unenforceable by a court, the remaining provisions of
+              these Terms will remain in effect. These Terms constitute the
+              entire agreement between us regarding our Service, and supersede
+              and replace any prior agreements we might have between us
+              regarding the Service. Changes We reserve the right, at our sole
+              discretion, to modify or replace these Terms at any time. If a
+              revision is material we will try to provide at least 30 days
+              notice prior to any new terms taking effect. What constitutes a
+              material change will be determined at our sole discretion. By
+              continuing to access or use our Service after those revisions
+              become effective, you agree to be bound by the revised terms. If
+              you do not agree to the new terms, please stop using the Service.
+              Contact Us If you have any questions about these Terms, please
+              contact us.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
